9369c06bd76e7ffcaa9733e40de3dac449aacbcf
Dziarkowska <anna-dziarkowska@wp.pl>
2019-04-11 01:40:03 +0200

    login+main screen

---
+++ app/src/main/java/com/example/smartroom/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.smartroom;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button = findViewById(R.id.login_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainScreenActivity();
+            }
+        });
+
+    }
+
+    public void openMainScreenActivity(){
+        Intent intent = new Intent(this, MainScreenActivity.class);
+        startActivity(intent);
+    }
+}
+
