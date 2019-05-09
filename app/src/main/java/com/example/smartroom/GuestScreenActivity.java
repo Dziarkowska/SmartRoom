a78fb5c50ff881a7dce3bba80080f9c6da6bf687
Dziarkowska <anna-dziarkowska@wp.pl>
2019-05-09 14:37:19 +0200

    guest main screen

--- app/src/main/java/com/example/smartroom/GuestScreenActivity.java
+++ app/src/main/java/com/example/smartroom/GuestScreenActivity.java
@@ -2,12 +2,17 @@ package com.example.smartroom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class GuestScreenActivity extends AppCompatActivity {
+
+    private Button vote_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_screen);
+        vote_btn = findViewById(R.id.vote_btn);
+        vote_btn.getBackground().setAlpha(128);
     }
 }
