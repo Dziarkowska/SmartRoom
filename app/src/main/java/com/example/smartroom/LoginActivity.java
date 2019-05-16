2368833b4652dc77071b89716e36c324d6e19313
Krystian Bytnar <krystian.bytnar@comarch.com>
2019-05-16 21:36:52 +0200

    Add login and password send to server

--- app/src/main/java/com/example/smartroom/LoginActivity.java
+++ app/src/main/java/com/example/smartroom/LoginActivity.java
@@ -5,6 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +30,24 @@ public class LoginActivity extends AppCompatActivity {
                 openMainScreenActivity();
             }
         });
+
+    }
+
+    public void onClick(View view){
+        EditText loginEt = (EditText) findViewById(R.id.login);
+        String login = loginEt.getEditableText().toString();
+        String url = "http://ec2-35-156-20-198.eu-central-1.compute.amazonaws.com:6969/app-connection-core/loginuser/ff";
+
+        EditText passwordEt = (EditText) findViewById(R.id.password);
+        String password = loginEt.getEditableText().toString();
+
+        String jsonString = "{" + "login: " + login + ", " + "password: " + password + "}";
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(url);
+        Invocation.Builder request = target.request();
+
+        Response response = request.post(Entity.entity(jsonString, MediaType.APPLICATION_JSON_TYPE));
 
     }
 
