f6f48a7979815e08bff511ae3e60864e49047d76
Dziarkowska <anna-dziarkowska@wp.pl>
2019-05-29 23:11:00 +0200

    vote message

--- app/src/main/java/com/example/smartroom/VoteActivity.java
+++ app/src/main/java/com/example/smartroom/VoteActivity.java
@@ -2,8 +2,10 @@ package com.example.smartroom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.Toast;
 
 import static android.view.Window.FEATURE_NO_TITLE;
 
@@ -23,7 +25,34 @@ public class VoteActivity extends AppCompatActivity {
 
         cooler_btn.getBackground().setAlpha(128);
         warmer_btn.getBackground().setAlpha(128);
-        nochanges_btn.getBackground().setAlpha(128);
+        nochanges_btn.getBackground().setAlpha(128); //ustawianie przezroczystosci
+
+        cooler_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //tu pewnie bedzie jakies zliczanie glosow
+                Toast.makeText(getBaseContext(),"Thanks for the vote!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        warmer_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //tu pewnie bedzie jakies zliczanie glosow
+                Toast.makeText(getBaseContext(),"Thanks for the vote!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        nochanges_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //tu pewnie bedzie jakies zliczanie glosow
+                Toast.makeText(getBaseContext(),"Thanks for the vote!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
 
     }
 }
